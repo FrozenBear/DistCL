a4ab0c265304cc355bdc8b2355ac95907c05bcf3
Nikita Popov <nikita@poksh.ru>
2014-11-22 14:02:40 +0600

    build fix
    removed project reference from Client to Core

--- DistCL.Client/Program.cs
+++ DistCL.Client/Program.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Security.AccessControl;
 using System.ServiceModel;
 using DistCL.Client.CompileService;
 using DistCL.Utils;
